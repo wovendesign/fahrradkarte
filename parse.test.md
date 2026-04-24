e440a5e9a2d2529d3b09f2448e3198f3d8126c24
Eric Wätke <email@ericwaetke.com>
2026-04-24 15:20:47 +0200

    Integrate MapLibre and add HTML parser

---
+++ parse.test.md
@@ -0,0 +1,32 @@
+Abschnittsnummer
+
+# Basisdaten
+
+Straße: Gutenbergstraße
+Abschnitt: zw. Hans-Thoma-Straße und Berliner Straße
+
+Lage: innerorts, angebaut
+Radnetzfunktion: Plusroute
+Länge in Meter: 189
+
+# Bestand Kfz-Verkehr
+Summe Kfz in der maflgeblichen Spitzenstunde: 1600
+Kfz-Parken Aufstellform: Parken nicht vorhanden oder nicht relevant
+Sicherheitsabstand zum Parken in Meter: Parken nicht vorhanden oder nicht relevant
+Zulässige Höchstgeschwindigkeit in km/h: 30/50
+
+# Bestand Radverkehr
+Führungsform Radverkehr: Radweg
+Bewertung der Führungsform nach Schulnoten: 1
+Bewertung des Anlagenzustands nach Schulnoten: 6
+Gesamtbewertung nach Schulnoten: 6
+Verkehrssicherheit: keine Auffälligkeit
+
+# Empfehlungen
+Maßnahmen: Bau eines Zweirichtungsradwegs auf der Südseite inklusive Überleitung in die Gutenbergstrafle.
+Kommentar: viele Geisterradler in Fahrtrichtung Innenstadt. Für die Umsetzung ist Grunderwerb notwendig.
+Priorität bei der Umsetzung:
+  - Radnetzfunktion: 3
+  - Bewertung Bestand Differenz Soll-Ist: 3
+  - Radverkehrsmengen: 4
+  - Priorität I: 10
--- src/App.svelte
+++ src/App.svelte
@@ -1,89 +1,32 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import heroImg from './assets/hero.png'
-  import Counter from './lib/Counter.svelte'
+  import MapLibre from 'svelte-maplibre/MapLibre.svelte';
+  import GeoJSON from 'svelte-maplibre/GeoJSON.svelte';
+  import LineLayer from 'svelte-maplibre/LineLayer.svelte';
+  import zielnetz from './radverkehrskonzept_zielnetz.geojson?url';
 </script>
 
-<section id="center">
-  <div class="hero">
-    <img src={heroImg} class="base" width="170" height="179" alt="" />
-    <img src={svelteLogo} class="framework" alt="Svelte logo" />
-    <img src={viteLogo} class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/App.svelte</code> and save to test <code>HMR</code></p>
-  </div>
-  <Counter />
-</section>
+<MapLibre
+  center={[13.08, 52.415]}
+  zoom={12}
+  class="map"
+  standardControls
+  style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+>
+  <GeoJSON data={zielnetz}>
+    <LineLayer
+      layout={{ 'line-cap': 'round', 'line-join': 'round' }}
+      paint={{
+        'line-width': 4,
+        'line-color': '#0066cc',
+      }}
+    />
+  </GeoJSON>
+</MapLibre>
 
-<div class="ticks"></div>
+<style>
+  :global(.map) {
+    height: 100vh;
+    width: 100%;
+  }
+</style>
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true">
-      <use href="/icons.svg#documentation-icon"></use>
-    </svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank" rel="noreferrer">
-          <img class="logo" src={viteLogo} alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://svelte.dev/" target="_blank" rel="noreferrer">
-          <img class="button-icon" src={svelteLogo} alt="" />
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true">
-      <use href="/icons.svg#social-icon"></use>
-    </svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li>
-        <a href="https://github.com/vitejs/vite" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#github-icon"></use>
-          </svg>
-          GitHub
-        </a>
-      </li>
-      <li>
-        <a href="https://chat.vite.dev/" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#discord-icon"></use>
-          </svg>
-          Discord
-        </a>
-      </li>
-      <li>
-        <a href="https://x.com/vite_js" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#x-icon"></use>
-          </svg>
-          X.com
-        </a>
-      </li>
-      <li>
-        <a href="https://bsky.app/profile/vite.dev" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#bluesky-icon"></use>
-          </svg>
-          Bluesky
-        </a>
-      </li>
-    </ul>
-  </div>
-</section>
-
-<div class="ticks"></div>
-<section id="spacer"></section>
