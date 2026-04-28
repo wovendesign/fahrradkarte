22339b47c94352c3ed0967a43392af9fe61c5235
Eric Wätke <email@ericwaetke.com>
2026-04-28 21:16:13 +0200

    fixed paths

--- fix-paths.ts
+++ fix-paths.ts
@@ -8,6 +8,17 @@ async function main() {
 		(path) => path.properties.id !== "x",
 	);
 
+	geojson.features = geojson.features.map((path) => {
+		return {
+			...path,
+			properties: {
+				id: path.properties.id ? String(path.properties.id) : undefined,
+				straße: path.properties.name || path.properties.straße,
+				abschnitt: path.properties.Zwischen || path.properties.abschnitt,
+			},
+		};
+	});
+
 	await Bun.write(
 		"src/radverkehrskonzept_zielnetz.geojson",
 		JSON.stringify(geojson, null, 2),
