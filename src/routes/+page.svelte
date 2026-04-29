7a1474df751ce20f29cbd5c1a5302bbdf8494c64
Eric Wätke <email@ericwaetke.com>
2026-04-29 14:40:53 +0200

    feat: re-open modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,9 @@
 		MapLibre,
 		SymbolLayer,
 		AttributionControl,
+		Control,
+    ControlButton,
+    ControlGroup
 	} from "svelte-maplibre";
 	import Sidebar from "$lib/Sidebar.svelte";
 	import data from "../data.json?url";
@@ -435,6 +438,13 @@
 			>
 				<NavigationControl position="top-left" />
 				<ScaleControl position="bottom-left" />
+				<Control position="top-left">
+    <ControlGroup>
+      <ControlButton onclick={() => {showModal = true}}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M196,96c0,29.47-24.21,54.05-56,59.06V156a12,12,0,0,1-24,0V144a12,12,0,0,1,12-12c24.26,0,44-16.15,44-36s-19.74-36-44-36S84,76.15,84,96a12,12,0,0,1-24,0c0-33.08,30.5-60,68-60S196,62.92,196,96Zm-68,92a20,20,0,1,0,20,20A20,20,0,0,0,128,188Z"></path></svg>
+      </ControlButton>
+    </ControlGroup>
+  </Control>
 				<!-- TODO: New Controll Button mit Fragezeichen was Modal triggered -->
 				<AttributionControl
 					position="bottom-right"
